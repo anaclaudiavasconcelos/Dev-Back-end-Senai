f7586296bb6ff0cfd7b84f6e3d309aa21febf3ee
anaclaudiavasconcelos <anaclaudia32511@gmail.com>
2026-07-17 21:55:13 -0300

    Adicionar arquivos de projeto.

---
+++ Lógica de Programação e Algoritmos/Aula01/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI";
+char caracter = 'S';
+bool logica = true;
+bool logica2 = false;
+
+WriteLine(palavra);
+
+WriteLine($"SENAI AMERICANA {palavra}");
+WriteLine("Curso DEV Backend");
+Write("Argentina ");
+Write("Argentina");
+
+ReadKey();
