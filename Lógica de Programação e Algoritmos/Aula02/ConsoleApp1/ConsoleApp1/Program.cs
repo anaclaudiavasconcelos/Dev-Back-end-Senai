b7fc1ba5b909f6279c6356ce16fcd35cceda03f0
anaclaudiavasconcelos <anaclaudia32511@gmail.com>
2026-07-20 21:53:44 -0300

    Declaração de variàveis, entrada e processamento  de dados

---
+++ Lógica de Programação e Algoritmos/Aula02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Declaração de variáveis
+double largura;
+double comprimento;
+double valor;
+double area;
+double preco;
+
+Console.Write("Digite a largura do terreno: ");
+largura = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a largura do terreno: ");
+comprimento = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do metro quadrado do terreno: ");
+valor = double.Parse(Console.ReadLine());
+
+
+//Processamento de dados
+area = largura * comprimento;
+preco = area * valor;
+
+//Saída de dados
+Console.WriteLine($"A área do terreno é: {area:n}\nPreço do terreno: R$ {preco:n}");
