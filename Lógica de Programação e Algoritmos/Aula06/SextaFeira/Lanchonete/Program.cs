e7488d09b46e107534851ea51ef5cba61d78999d
anaclaudiavasconcelos <anaclaudia32511@gmail.com>
2026-07-31 21:13:15 -0300

    Atividade_03 - Estrutura de repetição

---
+++ Lógica de Programação e Algoritmos/Aula06/SextaFeira/Lanchonete/Program.cs
@@ -0,0 +1,44 @@
+﻿//Declaração de variáveis
+int codigo, quantidade;
+double preco;
+
+//Entrada de dados
+Console.Write("Digite o código do produto: ");
+codigo = int.Parse(Console.ReadLine());
+Console.Write("Digite a quantidade do produto: ");
+quantidade = int.Parse(Console.ReadLine());
+
+//Estrutura de controle de seleção - switch case
+switch (codigo)
+{
+    case 1:
+        //Produto 1 - R$ 5.00
+        Console.WriteLine($"Valor a pagar: R$ {quantidade * 5.00}");
+        break;
+
+    case 2:
+        //Produto 2 - R$ 3.50
+        Console.WriteLine($"Valor a pagar: R$ {quantidade * 3.50}");
+        break;
+
+    case 3:
+        //Produto 3 - R$ 4.80
+        Console.WriteLine($"Valor a pagar: R$ {quantidade * 4.80}");
+        break;
+
+    case 4:
+        //Produto 4 - R$ 8.90
+        Console.WriteLine($"Valor a pagar: R$ {quantidade * 8.90}");
+        break;
+
+    case 5:
+        //Produto 5 - R$ 7.32
+        Console.WriteLine($"Valor a pagar: R$ {quantidade * 7.32}");
+        break;
+
+    default:
+        Console.WriteLine("Código invalido, tente novamente!");
+        break;
+
+} 
+Console.ReadLine();
---
+++ Lógica de Programação e Algoritmos/Aula06/SextaFeira/ParqueDeDiversao/Program.cs
@@ -0,0 +1,31 @@
+﻿//Declaração de variáveis
+int contagem = 0;
+try
+{
+    Console.Write("Digite a quantidade de visitantes: ");
+    int nvisitantes = int.Parse(Console.ReadLine());
+    Console.Write("Digite a altura mínima permitida (em cm): ");
+    double altura_minima = double.Parse(Console.ReadLine());
+    Console.Write("Digitea altura máxima permitida (em cm): ");
+    double altura_maxima = double.Parse(Console.ReadLine());
+
+    for(int i = 1; i<=nvisitantes; i++)
+    {
+        Console.Write($"Digite a altura do visitante número {i} (em cm):");
+        double alturadigitada = double.Parse(Console.ReadLine());
+
+       if( alturadigitada >= altura_minima & alturadigitada <= altura_maxima)
+        {
+            contagem++;
+        }
+    }
+
+    Console.WriteLine($"Total de pessoas que podem entrar na montanha-russa: {contagem} ");
+        
+}
+catch
+{
+    Console.WriteLine("Valor inválido. Digite um número inteiro.");
+}
+
+Console.ReadLine();
